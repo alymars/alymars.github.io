71091ccf8009c9c78b11f7aa670fd9bd45831a86
Alyssa Marshall <40774766+alymars@users.noreply.github.com>
2023-02-23 23:57:25 -0600

    Formatting changes only

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,24 @@ console.log("Hi! Welcome to my portfolio!");
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+        x.className += ' responsive';
     } else {
-    x.className = 'navtoggle';
+        x.className = 'navtoggle';
     }
 }
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
-} else {
-    mybutton.style.display = "none";
-}
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
